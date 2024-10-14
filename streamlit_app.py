4f7bc73a1ef14a4b2ba845ca52c1f0a0023cb7a6
Shuhua Xu <xshlcc@gmail.com>
2024-10-14 00:00:44 -0400

    read_in_data

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,126 +2,118 @@ import altair as alt
 import pandas as pd
 import streamlit as st
 
-### P1.2 ###
-
 @st.cache_data
 def load_data():
-    ## {{ CODE HERE }} ##
-    # Move this code into `load_data` function {{
-    cancer_df = pd.read_csv("https://raw.githubusercontent.com/hms-dbmi/bmi706-2022/main/cancer_data/cancer_ICD10.csv").melt(  # type: ignore
-        id_vars=["Country", "Year", "Cancer", "Sex"],
-        var_name="Age",
-        value_name="Deaths",
-    )
+    fid_df = pd.read_csv("VIW_FID.csv",low_memory=False,encoding = 'utf_8_sig')
 
-    pop_df = pd.read_csv("https://raw.githubusercontent.com/hms-dbmi/bmi706-2022/main/cancer_data/population.csv").melt(  # type: ignore
-        id_vars=["Country", "Year", "Sex"],
-        var_name="Age",
-        value_name="Pop",
-    )
+    flu_df = pd.read_csv("VIW_FNT - VIW_FNT.csv",low_memory=False,encoding = 'utf_8_sig')
+    flu_df = flu_df.dropna(subset=['FLUSEASON','ISOYW'],axis=0)
+    flu_df.loc[flu_df['ISOYW']=='MK','ISO2'] = 'MK'
+    flu_df.loc[flu_df['ISOYW']=='MK','ISOYW'] = flu_df.loc[flu_df['ISOYW']=='MK','MMWRYW']
+    flu_df = flu_df.astype({'ISOYW': 'float64'})
 
-    df = pd.merge(left=cancer_df, right=pop_df, how="left")
-    df["Pop"] = df.groupby(["Country", "Sex", "Age"])["Pop"].fillna(method="bfill")
-    df.dropna(inplace=True)
-
-    df = df.groupby(["Country", "Year", "Cancer", "Age", "Sex"]).sum().reset_index()
-    df["Rate"] = df["Deaths"] / df["Pop"] * 100_000
-# }}
-    return df
+    return fid_df, flu_df
 
 
-# Uncomment the next line when finished
-df = load_data()
-
-### P1.2 ###
+fid_df, flu_df = load_data()
 
 
-st.write("## Age-specific cancer mortality rates")
 
-### P2.1 ###
-# replace with st.slider
-year = st.slider('Year',min_value=df['Year'].min(),max_value=df['Year'].max(),value=2012)
-### P2.1 ###
+# vis 3
+year = st.slider('Year',min_value=flu_df['ISO_YEAR'].min(),max_value=flu_df['ISO_YEAR'].max(),value=2012)
 
+all_options_who_region = st.checkbox("Select all options for WHO Region",value=True)
 
-### P2.2 ###
-# replace with st.radio
-sex = st.radio('Sex',['M','F'])
-### P2.2 ###
+container_who_region = st.container()
+ 
+if all_options_who_region:
+    who_region = container_who_region.multiselect("WHO Region",
+         flu_df['WHOREGION'].dropna().unique(),flu_df['WHOREGION'].dropna().unique())
+else:
+    who_region = container_who_region.multiselect("WHO Region",
+        flu_df['WHOREGION'].dropna().unique())
 
+#influenza_a_types = ['AH1N12009','AH1','AH3','AH5']
 
-### P2.3 ###
-# replace with st.multiselect
-# (hint: can use current hard-coded values below as as `default` for selector)
-countries = st.multiselect('Countries',df["Country"].unique(),default=[
-    "Austria",
-    "Germany",
-    "Iceland",
-    "Spain",
-    "Sweden",
-    "Thailand",
-    "Turkey",
-])
-### P2.3 ###
+#flu_df_melted_influ_subtypes = 
+# ### P2.2 ###
+# # replace with st.radio
+# sex = st.radio('Sex',['M','F'])
+# ### P2.2 ###
 
 
-### P2.4 ###
-# replace with st.selectbox
-cancer = st.selectbox('Cancer',df['Cancer'].unique())
-### P2.4 ###
+# ### P2.3 ###
+# # replace with st.multiselect
+# # (hint: can use current hard-coded values below as as `default` for selector)
+# countries = st.multiselect('Countries',df["Country"].unique(),default=[
+#     "Austria",
+#     "Germany",
+#     "Iceland",
+#     "Spain",
+#     "Sweden",
+#     "Thailand",
+#     "Turkey",
+# ])
+# ### P2.3 ###
 
 
-### P2.5 ###
-ages = [
-    "Age <5",
-    "Age 5-14",
-    "Age 15-24",
-    "Age 25-34",
-    "Age 35-44",
-    "Age 45-54",
-    "Age 55-64",
-    "Age >64",
-]
-
-filtered_df = df[(df['Year']==year) & (df['Sex']==sex) & (df['Country'].isin(countries)) & (df['Cancer']==cancer)]
-
-del df
-
-brush = alt.selection_interval(encodings=['x'],name='select_age')
-
-chart = (alt.Chart().mark_rect().encode(
-    x=alt.X("Age", sort=ages),
-    y=alt.Y("Country"),
-    color=alt.Color('Rate:Q',
-                    scale=alt.Scale(type='log', domain=[0.01,1000], 
-                    clamp=True, 
-                    scheme="blues",reverse=True),
-                    title='Mortality rate per 100k',
-                    ),
-    tooltip=["Rate:Q"],
-).properties(
-    title=f"{cancer} mortality rates for {'males' if sex == 'M' else 'females'} in {year}",
-).add_params(brush))
-
-pop = (alt.Chart().mark_bar().encode(
-    x=alt.X('sum(Pop):Q'),
-    y=alt.Y('Country:N',sort='-x'),
-    color=alt.value("#87CEFA"),
-    tooltip=[alt.Tooltip("sum(Pop):Q"),
-        alt.Tooltip("Country:N")]
-).transform_filter(brush))
-
-combined = alt.vconcat(chart, pop, data=filtered_df)
-
-### P2.5 ###
-
-st.altair_chart(combined, use_container_width=True)
+# ### P2.4 ###
+# # replace with st.selectbox
+# cancer = st.selectbox('Cancer',df['Cancer'].unique())
+# ### P2.4 ###
 
 
-countries_in_subset = filtered_df["Country"].unique()
-if len(countries_in_subset) != len(countries):
-     if len(countries_in_subset) == 0:
-         st.write("No data avaiable for given subset.")
-     else:
-         missing = set(countries) - set(countries_in_subset)
-         st.write("No data available for " + ", ".join(missing) + ".")
+# ### P2.5 ###
+# ages = [
+#     "Age <5",
+#     "Age 5-14",
+#     "Age 15-24",
+#     "Age 25-34",
+#     "Age 35-44",
+#     "Age 45-54",
+#     "Age 55-64",
+#     "Age >64",
+# ]
+
+# filtered_df = df[(df['Year']==year) & (df['Sex']==sex) & (df['Country'].isin(countries)) & (df['Cancer']==cancer)]
+
+# del df
+
+# brush = alt.selection_interval(encodings=['x'],name='select_age')
+
+# chart = (alt.Chart().mark_rect().encode(
+#     x=alt.X("Age", sort=ages),
+#     y=alt.Y("Country"),
+#     color=alt.Color('Rate:Q',
+#                     scale=alt.Scale(type='log', domain=[0.01,1000], 
+#                     clamp=True, 
+#                     scheme="blues",reverse=True),
+#                     title='Mortality rate per 100k',
+#                     ),
+#     tooltip=["Rate:Q"],
+# ).properties(
+#     title=f"{cancer} mortality rates for {'males' if sex == 'M' else 'females'} in {year}",
+# ).add_params(brush))
+
+# pop = (alt.Chart().mark_bar().encode(
+#     x=alt.X('sum(Pop):Q'),
+#     y=alt.Y('Country:N',sort='-x'),
+#     color=alt.value("#87CEFA"),
+#     tooltip=[alt.Tooltip("sum(Pop):Q"),
+#         alt.Tooltip("Country:N")]
+# ).transform_filter(brush))
+
+# combined = alt.vconcat(chart, pop, data=filtered_df)
+
+# ### P2.5 ###
+
+# st.altair_chart(combined, use_container_width=True)
+
+
+# countries_in_subset = filtered_df["Country"].unique()
+# if len(countries_in_subset) != len(countries):
+#      if len(countries_in_subset) == 0:
+#          st.write("No data avaiable for given subset.")
+#      else:
+#          missing = set(countries) - set(countries_in_subset)
+#          st.write("No data available for " + ", ".join(missing) + ".")
